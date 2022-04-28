bbec3c1598a2ca31371bf2068ea9e692b0ab52c7
Morgan Fell <morgan@morganfell.dev>
2022-04-28 19:14:58 +0100

    Changed secret key to application setting.

--- hakuracloud/settings.py
+++ hakuracloud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x04m0olfoz2--x)21q1)pw1xn%nfxy!&h%1v&kmz+r(c^2#p9g'
+SECRET_KEY = getenv("HAKURA_SERVER_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
