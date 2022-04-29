04f14bdfc4f3d5807e572bcb0025da4a5aa5113b
Morgan Fell <morgan@morganfell.dev>
2022-04-29 16:59:18 +0100

    Added Follow model

--- hakura/models.py
+++ hakura/models.py
@@ -18,3 +18,9 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
     PostContent = models.CharField(max_length=140)
+
+
+class Follow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
